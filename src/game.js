61f7778f48bd7503a25eabdc47237dce95fb7a81
Jasmine <jsjohn026@gmail.com>
2019-12-09 12:57:35 -0600

    edit instructions

--- src/game.js
+++ src/game.js
@@ -83,6 +83,8 @@ background.onload = function() {
                     ctx.strokeStyle = "#FFFFFF";
                     ctx.font = "30px Arial";
                     ctx.strokeText("Eat as many cookies as you can before they destroy you!", 100, 250);
+                    ctx.strokeStyle = "#FFFFFF";
+                    ctx.font = "30px Arial";
                     ctx.strokeText("Use Space to pause and Arrow keys to move.", 100, 250);
                     ctx.strokeText("Click here to start playing Cookie Run!", 100, 250);
 
